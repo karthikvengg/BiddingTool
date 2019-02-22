ad6af3e15b19f3ae2281b9392dca8abc9f0125e5
karthik <karthikv.engg@gmail.com>
2019-02-22 11:21:43 +0100

    Changing to Response Entity

--- src/main/java/com/mbr/controller/BiddingController.java
+++ src/main/java/com/mbr/controller/BiddingController.java
@@ -5,6 +5,7 @@ import com.mbr.model.bid.WinningBid;
 import com.mbr.model.configure.Campaign;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,14 +19,13 @@ public class BiddingController {
     private BiddingService biddingService;
 
     @PostMapping("/configure")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createCampaigns(@RequestBody List<Campaign> campaigns) {
+    public ResponseEntity<Void> createCampaigns(@RequestBody List<Campaign> campaigns) {
         biddingService.configure(campaigns);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/bid")
-    @ResponseStatus(HttpStatus.OK)
-    public List<WinningBid> getResponse(@RequestParam Map<String, String> rules) {
-        return biddingService.getWinningBids(rules);
+    public ResponseEntity<List<WinningBid>> getResponse(@RequestParam Map<String, String> rules) {
+        return new ResponseEntity<>(biddingService.getWinningBids(rules),HttpStatus.OK);
     }
 }
